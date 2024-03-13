27b7e2d6f73c51b02d2433f0ce06a6d2f82e84dc
GWHeo <gwheo91@gmail.com>
2024-03-14 01:12:48 +0900

    - chatbox

--- static/javascript/chat.js
+++ static/javascript/chat.js
@@ -1,3 +1,4 @@
+// chat box sizing & positioning
 const rightContent = document.getElementById('right-content');
 const footer = document.getElementById('footer');
 const chatBubble = document.getElementById('chatBubble');
@@ -6,6 +7,8 @@ const minimizeButton = document.getElementById('minimizeBtn');
 const chatHeader = document.getElementById('chatHeader');
 const chatBody = document.getElementById('chatBody');
 const chatFooter = document.getElementById('chatFooter');
+const chatInput = document.getElementById('chatInput');
+const sendBtn = document.getElementById('sendBtn');
 
 function chatMarginSetting(breakPoint) {
     var innerWidth = window.innerWidth;
@@ -21,6 +24,21 @@ function chatMarginSetting(breakPoint) {
     }
 }
 
+function chatBodySizing() {
+    var topMargin = document.getElementById('gnb').offsetHeight;
+    var bottomMargin = document.getElementById('footer').offsetHeight;
+
+
+
+    var contentHeight = innerHeight - topMargin - bottomMargin;
+    if (contentHeight <= 400) {
+        chatBody.style.height = `${contentHeight - chatHeader.offsetHeight - chatFooter.offsetHeight - 32}px`;
+    } else {
+        chatBody.style.height = '240px';
+    }
+    console.log(chatBody.style.height)
+}
+
 chatMarginSetting(992);
 
 window.onresize = function(e) {
@@ -28,14 +46,7 @@ window.onresize = function(e) {
     var bottomMargin = document.getElementById('footer').offsetHeight;
 
     chatMarginSetting(992);
-
-    var contentHeight = innerHeight - topMargin - bottomMargin;
-    console.log(contentHeight)
-    if (contentHeight <= 400) {
-        chatBody.style.height = `${contentHeight - chatHeader.offsetHeight - chatFooter.offsetHeight - 32}px`;
-    } else {
-        chatBody.style.height = '240px';
-    }
+    chatBodySizing();
 }
 
 chatBubble.addEventListener('click', () => {
@@ -48,3 +59,29 @@ minimizeButton.addEventListener('click', () => {
     chatBubble.style.display = 'flex';
 });
 
+chatInput.addEventListener('keyup', function(e) {
+    if (e.keyCode == 13) {
+        e.preventDefault();
+        sendBtn.click();
+    }
+})
+
+// chat message
+function sendMessage() {
+    text = chatInput.value;
+    chatBody.innerHTML += messageHtml;
+
+    var messageContainer = document.getElementsByClassName('chat-message');
+    messageContainer[messageContainer.length - 1].innerHTML = text + '<br>';
+    chatBody.scrollTo(0, chatBody.scrollHeight);
+    chatInput.value = '';
+    sendBtn.disabled = true;
+}
+
+function chatInputBtnControl() {
+    if (chatInput.value == '' || chatInput.value == null) {
+        sendBtn.disabled = true;
+    } else {
+        sendBtn.disabled = false;
+    }
+}

1df5c78109be300bc88e8ac194652a92d9de3bd4
GWHeo <gwheo91@gmail.com>
2024-04-14 21:36:18 +0900

    - admin 추가

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(RoomLog)
+class RoomLogAdmin(admin.ModelAdmin):
+    list_display = [
+        'date',
+        'room_code',
+        'creator',
+        'status'
+    ]
+    
+
+@admin.register(UserLog)
+class UserLogAdmin(admin.ModelAdmin):
+    list_display = [
+        'date',
+        'room_code',
+        'user',
+        'is_creator',
+        'status'
+    ]
+    
+
+@admin.register(ActionLog)
+class ActionLogAdmin(admin.ModelAdmin):
+    list_display = [
+        'date',
+        'room_code',
+        'mode'
+    ]
